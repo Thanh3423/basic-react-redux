79e0ab990c31125d92647c982618486665a1ad77
Thanh3423 <nguyenvanthnahtk05@mail.com>
2024-09-01 21:06:26 +0700

    Nesting component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,53 +1,14 @@
 import React from 'react';
+import UserInfor from './UserInfor';
 
 class MyComponent extends React.Component {
-    state = {
-        name: 'Thanh',
-        address: 'Thanh1',
-        age: 21
-    };
 
-    handleClick(event) {
-        console.log(".>>> Click me my button")
-        console.log("random ", Math.floor((Math.random() * 100) + 1))
-        //merge State => react class
-        this.setState({
-            name: 'Eric',
-            age: Math.floor((Math.random() * 100) + 1)
-            // address: 'Thanh1',
-            // age: 21
-        })
-
-        // this.setState({
-        //     age: Math.floor((Math.random() * 100) + 1)
-        // })
-    }
-
-    handleOnMoverOver(event) {
-        //console.log(event.pageX)
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
     //jsx: viết code javascript trong html
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input type='text'
-                        onChange={(event) => this.handleOnChangeInput(event)} />
-                    <button>Submit</button>
-                </form>
+
+                <UserInfor />
             </div>
         );
     }
