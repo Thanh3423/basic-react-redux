b18a7142f342895cadce9e4bb42b8a5649bc65af
Thanh3423 <nguyenvanthnahtk05@mail.com>
2024-09-01 21:59:38 +0700

    outputting List

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 class DisplayInfor extends React.Component {
     render() {
+
         //destructuring array/object
-        const { age, name } = this.props;
-        console.log(this.props);
+        const { listUsers } = this.props;
+        console.log(listUsers);
         //props=> vieets tatws cuar properties: truyen du lieu tu component cha sang con
         return (
             <div>
+                {listUsers.map((user, index) => {
+                    return (
+                        <div key={user.id}>
+                            <div>My name is {user.name}</div>
+                            <div>My age's {user.age}</div>
+                            <hr />
+                        </div>
+                    )
+                })}
+                {/* <div>My name is {name}</div>
+                <div>My age's {age}</div>
+                <hr />
                 <div>My name is {name}</div>
                 <div>My age's {age}</div>
+                <hr />
+                <div>My name is {name}</div>
+                <div>My age's {age}</div> */}
 
             </div>
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,17 +3,26 @@ import UserInfor from './UserInfor';
 import DisplayInfor from './DisplayInfor';
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            { id: 1, name: "Thanh", age: "21" },
+            { id: 2, name: "Thanh1", age: "231" },
+            { id: 3, name: "Thanh3", age: "24" },
+        ]
+    }
 
+    //DRY: don't repeat youseft
     //jsx: viết code javascript trong html
     render() {
+
         return (
             <div>
 
                 <UserInfor />
                 <br /><br />
-                <DisplayInfor name="Thanh" age="21" />
-                <hr />
-                <DisplayInfor name="Eric" age={"21"} />
+                <DisplayInfor
+                    listUsers={this.state.listUsers}
+                />
             </div>
         );
     }
