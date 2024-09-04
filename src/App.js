4059d9db60bb853013a36611782a9722dd2a1571
Thanh3423 <nguyenvanthnahtk05@mail.com>
2024-09-04 15:16:37 +0700

    Điều hướng trang với Link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,6 +10,19 @@ const App = () => {
 
     <div className="app-container">
       <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users"> go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admins"> go to admin page</Link>
+
+          </button>
+
+        </div>
+      </div>
     </div>
   );
 }
